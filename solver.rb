657972e77c2549bbc5b723d6ac7bcfe560ef7d92
Elson Otake <elsonotake@yahoo.com.br>
2022-09-01 17:13:28 -0300

    Update method to return fizzbuzz if input is divisible by 3 and 5

--- solver.rb
+++ solver.rb
@@ -14,6 +14,7 @@ class Solver
   end
 
   def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
     return 'fizz' if (num % 3).zero?
     return 'buzz' if (num % 5).zero?
   end
