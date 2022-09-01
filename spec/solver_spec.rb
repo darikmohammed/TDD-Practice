173668d289e027976154cbf09ac0bd98ceb1d2fd
darikmohammed <r.m.darik@gmail.com>
2022-09-01 22:51:44 +0300

    Solve #new describe test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,7 +1,10 @@
+require_relative '../solver'
+
 describe Solver do
   before :each do
     @solver = Solver.new
   end
+
   describe '#new' do
     it "Has to be instance of Solver" do
       expect(@solver).to be_instance_of Solver
